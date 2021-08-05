a2cbfe4d170973ea7b7c5cc2e7b4bd2fa26f7c4a
alirezasyed <codingchallenge2020@yahoo.com>
2021-08-05 15:16:12 -0700

    ajoute la forme en html

--- index.php
+++ index.php
@@ -13,7 +13,26 @@
 
 <body>
     
+  <form action="" method="POST">
+    <header>Télécharger</header>
 
+    <div class="url-input">
+      <span class="title">Coller l'URL de la vidéo :</span>
+      <div class="field">
+        <input type="text" placeholder="https://www.youtube.com/watch?v=lqwdD2ivIbM" required>
+        <input class="hidden-input" type="hidden" name="imgurl">
+        <span class="bottom-line"></span>
+      </div>
+    </div>
+
+    <div class="preview-area">
+      <img class="thumbnail" src="" alt="">
+      <i class="icon fas fa-cloud-download-alt"></i>
+      <span>Collez l'URL de la vidéo pour voir l'aperçu </span>
+    </div>
+    
+    <button class="download-btn" type="submit" name="button">Télécharger</button>
+  </form>
 
 </body>
 </html>

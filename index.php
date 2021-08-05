768e6bc9c3190a7f58821c7dbd3b5e9968a184f6
alirezasyed <codingchallenge2020@yahoo.com>
2021-08-05 14:42:09 -0700

    initialise le petit projet de videoConverter en php et javascript

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!-- Coding By CodingNepal - youtube.com/codingnepal -->
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Télécharger la vidéo YouTube </title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
+</head>
+
+<body>
+    
+
+
+</body>
+</html>
